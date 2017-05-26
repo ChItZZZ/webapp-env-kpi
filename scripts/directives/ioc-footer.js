483a7e940a712e687fe932e03b4d2b029b9e874d
ChItZZZ <648267970@qq.com>
2017-05-27 00:53:21 +0800

    delete footer

--- scripts/directives/ioc-footer.js
+++ scripts/directives/ioc-footer.js
@@ -6,13 +6,3 @@
  * @description
  * # iocFooter
  */
-angular.module('app')
-    .directive('iocFooter', function () {
-        return {
-            templateUrl: '/template/ioc-footer.html',
-            restrict: 'EA',
-            link: function postLink(scope, element, attrs) {
-
-            }
-        };
-    });
